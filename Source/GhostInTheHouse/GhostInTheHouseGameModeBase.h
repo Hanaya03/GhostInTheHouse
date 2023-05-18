1bc3acae06768744d31804e2326a7195f80d7469
Hanaya03 <ha4863519@gmail.com>
2023-05-18 13:48:45 -0700

    initial commit
    Created the unreal project: GhostInTheHouse

---
+++ Source/GhostInTheHouse/GhostInTheHouseGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GhostInTheHouseGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GHOSTINTHEHOUSE_API AGhostInTheHouseGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
